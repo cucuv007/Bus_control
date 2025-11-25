0fa68b352beee430739036a878f6d0f46b0b8932
Utku Esin KURUCU <utkuesin.kurucu@abb.local>
2025-11-25 16:54:08 +0300

    Dosyalar güncellendi

--- pages/api/change-password.js
+++ pages/api/change-password.js
@@ -1,9 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
-
-const supabase = createClient(supabaseUrl, supabaseServiceKey);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 export default async function handler(req, res) {
   // Only allow POST method
